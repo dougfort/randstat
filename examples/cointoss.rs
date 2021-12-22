d4208e33605546d72d2d7edab0654ae522af641a
dougfort <doug.fort@gmail.com>
2021-12-22 14:23:59 -0500

    fix up the cointoss example

--- examples/cointoss.rs
+++ examples/cointoss.rs
@@ -1,3 +1,25 @@
+#[derive(Debug)]
+#[repr(u8)]
+enum Coin {
+    Tails = 0,
+    Heads = 1,
+}
+
+impl From<u8> for Coin {
+
+    fn from(i: u8) -> Self {
+        match  i {
+            0x00 => Coin::Tails,
+            0x01 => Coin::Heads,
+            _ => panic!("unknown Coin {}", i),
+        }
+    }
+}
+
 fn main() {
-    println!("cointoss");
+    let init = vec![randstat::StatInit{percentage:  50, value: Coin::Heads as u8}];
+    let rs: randstat::RandStat = randstat::RandStat::new(&init).unwrap();
+    for c in rs.take(100).map(Coin::from) {
+        println!("{:?}", c);
+    }
 }
